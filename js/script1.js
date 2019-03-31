72ea7230c5b4d80829b4e0e95145e087f1b3b5d3
yvonne <yvonnegax98@gmail.com>
2019-04-01 01:12:41 +0300

    forgot to add the right script path so i have changed it

--- js/script1.js
+++ js/script1.js
@@ -1,52 +1,55 @@
-function check() {
-  var q1 = document.test.q1.value;
-  var q2 = document.test.q2.value;
-  var q3 = document.test.q3.value;
-  var q4 = document.test.q4.value;
-  var q5 = document.test.q5.value;
+function check(){
+    var q1=document.test.q1.value;
+    var q2=document.test.q2.value;
+    var q3=document.test.q3.value;
+    var q4=document.test.q4.value;
+    var q5=document.test.q5.value;
 
-  var score = 0;
+    var score = 0;
 
-  if (q1 == "c") {
-    score++;
-  }
-  if (q2 == "a") {
-    score++;
-  }
-  if (q3 == "d") {
-    score++;
-  }
-  if (q4 == "a") {
-    score++;
-  }
-  if (q5 == "c") {
-    score++;
-  }
-  var conversion = score / 5 * 100;
-  score = conversion;
 
-  var message = ["Great job!", "aim higher", "fairly done", "poorly done", "Get serious"];
-  var messageArray;
-  var picture = ["img/great-job.gif", "img/keep-up.gif", "img/fairly-done.gif", "img/poorly-done.gif", "img/get-serious.gif"];
+    if (q1== "c"){
+        score++;
+    }
+    if (q2== "a"){
+        score++;
+    }
+    if (q3== "d"){
+        score++;
+    }
+    if (q4== "a"){
+        score++;
+    }
+    if (q5== "c"){
+        score++;
+    }
+    //alert("your score " + score + " points");
+    var conversion = score/5 * 100;
+    score = conversion;
 
-  if (score < 40) {
-    messageArray = 4;
-  }
-  if (score >= 40 && score < 60) {
-    messageArray = 3;
-  }
-  if (score >= 60 && score < 80) {
-    messageArray = 2;
-  }
-  if (score >= 80 && score < 100) {
-    messageArray = 1;
-  }
-  if (score === 100) {
-    messageArray = 0;
-  }
+    var message = ["Great job!","aim higher","fairly done","poorly done","Get serious"];
+    var messageArray;
+    var picture =["img/great-job.gif","img/keep-up.gif","img/fairly-done.gif","img/poorly-done.gif","img/get-serious.gif"];
 
-  document.getElementById("after_submit").style.visibility = "visible";
-  document.getElementById("message").innerHTML = message[messageArray];
-  document.getElementById("number_correct").innerHTML = "You got " + score + "% correct.";
-  document.getElementById("picture").src = picture[messageArray];
+    if(score < 40 ){
+        messageArray =4;
+    }
+    if(score >= 40 && score < 60){
+        messageArray =3;
+    }
+    if(score >= 60 && score < 80){
+        messageArray =2;
+    }
+    if( score >= 80 && score <100){
+        messageArray =1;
+    }
+    if(score === 100 ){
+        messageArray =0;
+    }
+
+
+    document.getElementById("after_submit").style.visibility = "visible";
+    document.getElementById("message").innerHTML= message[messageArray];
+    document.getElementById("number_correct").innerHTML = "You got " + score + "% correct.";
+    document.getElementById("picture").src = picture[messageArray];
 }
