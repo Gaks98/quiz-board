5aa143d91ce143df4f0acab3c0288f178d34bf4e
yvonne <yvonnegax98@gmail.com>
2019-03-31 22:20:27 +0300

    created a function check() and created variables in it that represent each input's name and created if statement for each variable

---
+++ js/script1.js
@@ -0,0 +1,25 @@
+function check(){
+    var q1=document.test.q1.value;
+    var q2=document.test.q2.value;
+    var q3=document.test.q3.value;
+    var q4=document.test.q4.value;
+    var q5=document.test.q5.value;
+
+    var score = 0;
+
+
+    if (q1== "c"){
+        score++;
+    }
+    if (q2== "a"){
+        score++;
+    }
+    if (q3== "d"){
+        score++;
+    }
+    if (q4== "a"){
+        score++;
+    }
+    if (q5== "c"){
+        score++;
+    }
