3791f28802469279277cf338d277a07acaddd0a2
yvonne <yvonnegax98@gmail.com>
2019-03-31 23:12:05 +0300

    added if statements that will determine the users action on the login page if nothing/or an input is not inputed they get a sweet alert error message and if inputed they get a sweet alert success message

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -2,3 +2,14 @@ function validation(){
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var name =name.toUpperCase();
+
+    if (name===""||email===""){
+            swal("OOPS!", "Somebody didnt fill all inputs", "error");
+        }
+        else{
+        swal("Well Come! " + name ,
+        "Just right on time!", "success").then(function() {
+            window.location = "index2.html";
+        });
+        }
+    }
